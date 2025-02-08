7f1b42f27ed9d74c25ade3cbc9c43a4c52ba7e30
Soborak <maksymilian.soborak01@gmail.com>
2025-02-08 14:38:28 +0100

    Commit

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,35 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+
+        if (name == null || name.isBlank()  ) {
+            throw new IllegalArgumentException("You cannot create products with null or empty name.");
+        }
+
+        if (price == null || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("You cannot create products with null or negative price.");
+        }
+
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+
+        return price.add(price.multiply(taxPercent));
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+
+        return this.price.add(this.price.multiply(this.taxPercent));
     }
 }
